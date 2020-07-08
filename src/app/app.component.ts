f11280394669da53d3b05cd5fdbfe4dfb42357be
vinod <vinoth.it@gmail.com>
2020-07-08 19:43:33 +0530

    created angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LearningAngularApp';
+  updateTitle = "empty";
+
+  buttonClick() {
+    this.updateTitle = "updated";
+  }
 }
